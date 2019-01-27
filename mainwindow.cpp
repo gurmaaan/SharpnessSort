98b668e294c124951f8e008aec60388d1c317146
Dmitry <dmitry@gurov.co>
2019-01-27 17:44:04 +0300

    Исправлено вычетание Испралвено добавление зеленого цвета к индексу

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -25,8 +25,6 @@ void MainWindow::on_action_openDir_triggered()
 
     ui->dir_progress->setMaximum(imgNames.count());
     ui->tableView->setIconSize(QSize(100, 100));
-    //TODO : норм размер иконок
-    //ui->tableView->verticalHeader()->setDefaultSectionSize(100);
     ui->tableView->setRowHeight(0, 100);
 
     QList<QStandardItem*> imgRow;
@@ -45,18 +43,20 @@ void MainWindow::on_action_openDir_triggered()
         QStandardItem *nameItem = new QStandardItem(imgNames.at(i));
         nameItem->setData(Qt::AlignCenter, Qt::TextAlignmentRole);
 
-        QIcon icon(QPixmap::fromImage(img));
-        imgItem->setIcon(icon);
+        imgItem->setSizeHint(QSize(100, 100));
+        imgItem->setIcon(QPixmap::fromImage(img));
 
         imgRow << imgItem;
         nameRow << nameItem;
 
         ui->dir_progress->setValue(i+1);
     }
+
     ui->baseImg_cb->addItems(imgNames);
     _model->appendRow(imgRow);
     _model->appendRow(nameRow);
     setActiveImg(0);
+    setBaseImgPreview(_images.first());
 }
 
 void MainWindow::setupWidgets()
@@ -88,15 +88,28 @@ void MainWindow::setActiveImg(int index)
 {
     if( (index >= 0) && (index < _images.count()) )
     {
-        _activeIndex = index;
+        setVisibleRectCorners(ui->vie_gv->mapToScene(ui->vie_gv->viewport()->geometry()).boundingRect());
+        setActiveIndex(index);
         ui->tableView->selectColumn(index);
-        _viewScene->clear();
+
         QImage activeImg = _images.at(index);
-        QPixmap pixmap( QPixmap::fromImage(activeImg) );
-        QGraphicsPixmapItem *pixmapItem = new QGraphicsPixmapItem(pixmap);
-        _viewScene->addItem(pixmapItem);
-        //diffImages(_baseImage, activeImg);
+        setActiveImg(activeImg);
+        diffImages(_baseImage, activeImg);
     }
+}
+
+void MainWindow::setActiveImg(QImage img)
+{
+    _viewScene->clear();
+    QPixmap pixmap( QPixmap::fromImage(img) );
+    QGraphicsPixmapItem *pixmapItem = new QGraphicsPixmapItem(pixmap);
+    _viewScene->addItem(pixmapItem);
+}
+
+void MainWindow::setBaseImgPreview(QImage img)
+{
+    QPixmap pixMap = QPixmap::fromImage(img);
+    ui->baseImagPrevie_lbl->setPixmap(pixMap.scaled(250, 150));
 }
 
 void MainWindow::scaleImage(double k)
@@ -115,15 +128,46 @@ void MainWindow::diffImages(QImage base, QImage current)
         {
             for(int i = 0; i < base.width(); i++)
             {
-                int r = 0, g = 0, b = 0;
-                r = (( base.pixelColor(i,j).red() - current.pixelColor(i,j).red() ) * k) + 128;
-                g = (( base.pixelColor(i,j).green() - current.pixelColor(i,j).green() ) * k) + 128;
-                b = (( base.pixelColor(i,j).blue() - current.pixelColor(i,j).blue() ) * k) + 128;
+                int r = (( base.pixelColor(i,j).red() - current.pixelColor(i,j).red() ) * k) + 128;
+                int g = (( base.pixelColor(i,j).green() - current.pixelColor(i,j).green() ) * k) + 128;
+                int b = (( base.pixelColor(i,j).blue() - current.pixelColor(i,j).blue() ) * k) + 128;
+
+                QColor resClr = validColor(r, g, b);
+                resImg.setPixelColor(i, j, resClr);
             }
         }
-
         setImgDiff(resImg);
     }
+}
+
+void MainWindow::setVisibleRectCorners(QRectF visible)
+{
+    setSB(ui->tlx_sb, static_cast<int>(visible.topLeft().x()));
+    setSB(ui->tly_sb, static_cast<int>(visible.topLeft().y()));
+    setSB(ui->brx_sb, static_cast<int>(visible.bottomRight().x()));
+    setSB(ui->brx_sb, static_cast<int>(visible.bottomRight().y()));
+}
+
+QColor MainWindow::validColor(int r, int g, int b)
+{
+    int newR = 0, newG = 0, newB = 0;
+    newR = validComponent(r);
+    newG = validComponent(g);
+    newB = validComponent(b);
+    QColor outClr(newR, newG, newB);
+    return outClr;
+}
+
+int MainWindow::validComponent(int c)
+{
+    int outC = 0;
+    if(c < 0)
+        outC = 0;
+    else if(c > 255)
+        outC = 255;
+    else
+        outC = c;
+    return outC;
 }
 
 void MainWindow::on_action_exit_triggered()
@@ -133,12 +177,12 @@ void MainWindow::on_action_exit_triggered()
 
 void MainWindow::on_action_next_triggered()
 {
-    setActiveImg(_activeIndex + 1);
+    setActiveImg(getActiveIndex() + 1);
 }
 
 void MainWindow::on_action_previous_triggered()
 {
-    setActiveImg(_activeIndex - 1);
+    setActiveImg(getActiveIndex() - 1);
 }
 
 void MainWindow::on_action_first_triggered()
@@ -153,10 +197,15 @@ void MainWindow::on_action_last_triggered()
 
 void MainWindow::on_baseImg_cb_currentIndexChanged(int index)
 {
-    QImage baseImg = _images.at(index);
-    setBaseImage(baseImg);
-    QPixmap pixMap = QPixmap::fromImage(baseImg);
-    ui->baseImagPrevie_lbl->setPixmap(pixMap.scaled(250, 150));
+    if(index >= 0 && index < _images.count())
+    {
+        QImage baseImg = _images.at(index);
+        setBaseImage(baseImg);
+        setBaseImgPreview(baseImg);
+        for(int i = 0; i < _model->columnCount(); i++)
+           _model->setData(_model->index(1,i), QColor(Qt::white), Qt::BackgroundColorRole);
+        _model->setData(_model->index(1,index), QColor(Qt::green), Qt::BackgroundColorRole);
+    }
 }
 
 QImage MainWindow::getBaseImage() const
@@ -175,4 +224,53 @@ void MainWindow::setImgDiff(QImage result)
     QPixmap pm(QPixmap::fromImage(result));
     QGraphicsPixmapItem *pmItem = new QGraphicsPixmapItem(pm);
     _diffScene->addItem(pmItem);
+}
+
+void MainWindow::on_tableView_clicked(const QModelIndex &index)
+{
+   setActiveImg(index.column());
+}
+
+void MainWindow::on_MainWindow_customContextMenuRequested(const QPoint &pos)
+{
+    //TODO : доделать меню
+   QMenu *menu = new QMenu(this);
+   QAction *setActiveAction = new QAction("Установить опорным", this);
+   menu->addAction(setActiveAction);
+   menu->popup(ui->tableView->viewport()->mapToGlobal(pos));
+}
+
+void MainWindow::on_scale_sldr_sliderMoved(int position)
+{
+    double posD = static_cast<double>(position) / 10.0;
+    ui->scale_sb->setValue(posD);
+    if(_viewScene->items().count() > 0)
+    {
+        QImage img = _images.at(getActiveIndex());
+        if(position <= 5)
+        {
+//            QImage scaled = img.scaledToWidth(img.height() * position);
+//            QPixmap pixmap = QPixmap::fromImage(scaled);
+//            _viewScene->clear();
+
+//            QGraphicsPixmapItem *scaledItem = new QGraphicsPixmapItem(pixmap);
+//            _viewScene->addItem(scaledItem);
+        }
+        //QPixmap pm(QPixmap::fromImage(img.scaled(img.width() * position, img.height() * position)));
+    }
+}
+
+int MainWindow::getActiveIndex() const
+{
+    return _activeIndex;
+}
+
+void MainWindow::setActiveIndex(int activeIndex)
+{
+    _activeIndex = activeIndex;
+}
+
+void MainWindow::on_vie_gv_rubberBandChanged(const QRect &viewportRect, const QPointF &fromScenePoint, const QPointF &toScenePoint)
+{
+    qDebug() << viewportRect;
 }
--- mainwindow.h
+++ mainwindow.h
@@ -34,6 +34,9 @@ public:
     QImage getBaseImage() const;
     void setBaseImage(const QImage &value);
 
+    int getActiveIndex() const;
+    void setActiveIndex(int activeIndex);
+
 signals:
     void resultCalced(QImage result);
 
@@ -47,8 +50,11 @@ private slots:
     void on_action_previous_triggered();
     void on_action_first_triggered();
     void on_action_last_triggered();
-
     void on_baseImg_cb_currentIndexChanged(int index);
+    void on_tableView_clicked(const QModelIndex &index);
+    void on_MainWindow_customContextMenuRequested(const QPoint &pos);
+    void on_scale_sldr_sliderMoved(int position);
+    void on_vie_gv_rubberBandChanged(const QRect &viewportRect, const QPointF &fromScenePoint, const QPointF &toScenePoint);
 
 private:
     Ui::MainWindow *ui;
@@ -63,8 +69,13 @@ private:
     void connectAll();
     void setSB(QSpinBox *sb, int value);
     void setActiveImg(int index);
+    void setActiveImg(QImage img);
+    void setBaseImgPreview(QImage img);
     void scaleImage(double k);
     void diffImages(QImage base, QImage current);
+    void setVisibleRectCorners(QRectF visible);
+    QColor validColor(int r = 0, int g = 0, int b = 0);
+    int validComponent(int c = 0);
 };
 
 #endif // MAINWINDOW_H
